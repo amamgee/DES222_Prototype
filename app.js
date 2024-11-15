e3e418c2ff21f67d0128f66b38ed6e1a46f250ff
amamgee <a_g271@student.usc.edu.au>
2024-11-15 18:12:48 +0100

    updates

--- app.js
+++ app.js
@@ -136,21 +136,3 @@ document.addEventListener('DOMContentLoaded', () => {
   closeModalButton.addEventListener('click', closeModal);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  const likeButtons = document.querySelectorAll('.like-button');
-  
-  likeButtons.forEach(button => {
-    button.addEventListener('click', function() {
-      this.classList.toggle('liked'); // Toggle the 'liked' class to change the color
-    });
-  });
-});
-document.addEventListener('DOMContentLoaded', function() {
-  const likeButtons = document.querySelectorAll('.like-button');
-
-  likeButtons.forEach(button => {
-    button.addEventListener('click', function() {
-      this.classList.toggle('liked'); // Toggle the 'liked' class to change color
-    });
-  });
-});
